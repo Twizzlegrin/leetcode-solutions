d5028ab11a0013fd085ee41662f4b785c58dcd4c
Ruanga <raggy@live.no>
2023-08-29 20:34:12 +0200

    Added three more SQL problems

---
+++ 14_days_study_crack_algo/CrackSQLIn50/Basic_aggregte_functions/Immediate_food_delivery_II.sql
@@ -0,0 +1,8 @@
+SELECT 
+  ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) as immediate_percentage
+FROM 
+  delivery WHERE (customer_id, order_date) IN (SELECT customer_id, min(order_date) as first_order
+  FROM 
+    delivery
+group by 
+  customer_id)
